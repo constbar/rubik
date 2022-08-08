dbf998f20110a137a95eccb233a1f262ef49e1f5
lamb <lamb@gmail.com>
2022-08-08 20:26:50 +0300

    start layer-by-layer method

--- rubik.py
+++ rubik.py
@@ -1,23 +1,20 @@
 from __future__ import annotations
 from queue import PriorityQueue
 # state0 сделать как шафл
+# can @property be with __
 
 import copy
 from termcolor import colored
 from itertools import repeat
 from typing import Final
 import operator
+import pandas as pd
 
 gre = lambda i: colored(i, 'green')
 yll = lambda i: colored(i, 'yellow')
 
 
-class Solver:
-    def __init__(self, init_state):
-        self.init__state = init_state
-
-
-class State:  # rename like rubik state
+class State:  # rename like rubik state or cube state
     goal_edge_permutation = [0] * 12  # better list i belive
     goal_corner_permutation = '0' * 8
     goal_edge_orientation = '0' * 12
@@ -25,22 +22,20 @@ class State:  # rename like rubik state
     id = 0
     # delete None as default value
 
-    def __init__(self, properties, notation, notation_history):  # here add glubina poiska
+    def __init__(self, properties, stage, notation, notation_history):  # here add glubina poiska # nado li dobavit' None avtomatom?? # rename moves history
         self.properties = copy.deepcopy(properties)
         self.cp = self.properties['corner_permutation']
         self.ep = self.properties['edge_permutation']
         self.co = self.properties['corner_orientation']
         self.eo = self.properties['edge_orientation']
 
-        self.notation = notation
-        self.notation_history = notation_history
-        self.id = None
+        self.notation = notation  # if not none
+        self.notation_history = notation_history # if not none
+        self.id = None  # del # make it in right order
+        self.stage = 'stage_0' if stage is None else stage
 
         # self.notation_history = self.notation_history  # list or none
-        if notation_history is not None:
-            self.notation_history = notation_history
-        else:
-            self.notation_history = list()
+        self.notation_history = list() if notation_history is None else notation_history
 
         if self.notation:
             self.moves(self.notation)
@@ -57,42 +52,10 @@ class State:  # rename like rubik state
         search_depth = 0
 
     def moves(self, move):  # maybe rename it
-        """ the notation with the number 2 makes a double clockwise rotation.
-        apostrophe notation makes a counter-clockwise rotation. """
-        def u_clockwise():
-            self.cp[0], self.cp[7], self.cp[3], self.cp[4] = \
-                self.cp[4], self.cp[0], self.cp[7], self.cp[3]
-
-            self.ep[0], self.ep[11], self.ep[3], self.ep[8] = \
-                self.ep[8], self.ep[0], self.ep[11], self.ep[3]
-
-            self.co[self.cp[0]] = State.calculate_new_co(self.co[self.cp[0]] - 1)
-            self.co[self.cp[3]] = State.calculate_new_co(self.co[self.cp[3]] - 1)
-            self.co[self.cp[4]] = State.calculate_new_co(self.co[self.cp[4]] + 1)
-            self.co[self.cp[7]] = State.calculate_new_co(self.co[self.cp[7]] + 1)
-
-            self.eo[self.ep[0]] = (self.eo[self.ep[0]] + 1) % 2
-            self.eo[self.ep[3]] = (self.eo[self.ep[3]] + 1) % 2
-            self.eo[self.ep[8]] = (self.eo[self.ep[8]] + 1) % 2
-            self.eo[self.ep[11]] = (self.eo[self.ep[11]] + 1) % 2
-
-        def d_clockwise():
-            self.cp[1], self.cp[6], self.cp[2], self.cp[5] = \
-                self.cp[5], self.cp[1], self.cp[6], self.cp[2]
-
-            self.ep[1], self.ep[9], self.ep[2], self.ep[10] = \
-                self.ep[10], self.ep[1], self.ep[9], self.ep[2]
-
-            self.co[self.cp[1]] = State.calculate_new_co(self.co[self.cp[1]] - 1)
-            self.co[self.cp[2]] = State.calculate_new_co(self.co[self.cp[2]] - 1)
-            self.co[self.cp[5]] = State.calculate_new_co(self.co[self.cp[5]] + 1)
-            self.co[self.cp[6]] = State.calculate_new_co(self.co[self.cp[6]] + 1)
-
-            self.eo[self.ep[1]] = (self.eo[self.ep[1]] + 1) % 2
-            self.eo[self.ep[2]] = (self.eo[self.ep[2]] + 1) % 2
-            self.eo[self.ep[9]] = (self.eo[self.ep[9]] + 1) % 2
-            self.eo[self.ep[10]] = (self.eo[self.ep[10]] + 1) % 2
-
+        """ !!!!!!!!!!! add description
+        the notation with the number 2 makes a double clockwise rotation.
+        apostrophe notation makes a counter-clockwise rotation.
+        """
         def l_clockwise():
             self.cp[0], self.cp[4], self.cp[1], self.cp[5] = \
                 self.cp[5], self.cp[0], self.cp[4], self.cp[1]
@@ -131,13 +94,41 @@ class State:  # rename like rubik state
             self.co[self.cp[5]] = State.calculate_new_co(self.co[self.cp[5]] - 1)
             self.co[self.cp[7]] = State.calculate_new_co(self.co[self.cp[7]] - 1)
 
+        def u_clockwise():
+            self.cp[0], self.cp[7], self.cp[3], self.cp[4] = \
+                self.cp[4], self.cp[0], self.cp[7], self.cp[3]
+
+            self.ep[0], self.ep[11], self.ep[3], self.ep[8] = \
+                self.ep[8], self.ep[0], self.ep[11], self.ep[3]
+
+            self.co[self.cp[0]] = State.calculate_new_co(self.co[self.cp[0]] - 1)
+            self.co[self.cp[3]] = State.calculate_new_co(self.co[self.cp[3]] - 1)
+            self.co[self.cp[4]] = State.calculate_new_co(self.co[self.cp[4]] + 1)
+            self.co[self.cp[7]] = State.calculate_new_co(self.co[self.cp[7]] + 1)
+
+            self.eo[self.ep[0]] = (self.eo[self.ep[0]] + 1) % 2
+            self.eo[self.ep[3]] = (self.eo[self.ep[3]] + 1) % 2
+            self.eo[self.ep[8]] = (self.eo[self.ep[8]] + 1) % 2
+            self.eo[self.ep[11]] = (self.eo[self.ep[11]] + 1) % 2
+
+        def d_clockwise():
+            self.cp[1], self.cp[6], self.cp[2], self.cp[5] = \
+                self.cp[5], self.cp[1], self.cp[6], self.cp[2]
+
+            self.ep[1], self.ep[9], self.ep[2], self.ep[10] = \
+                self.ep[10], self.ep[1], self.ep[9], self.ep[2]
+
+            self.co[self.cp[1]] = State.calculate_new_co(self.co[self.cp[1]] - 1)
+            self.co[self.cp[2]] = State.calculate_new_co(self.co[self.cp[2]] - 1)
+            self.co[self.cp[5]] = State.calculate_new_co(self.co[self.cp[5]] + 1)
+            self.co[self.cp[6]] = State.calculate_new_co(self.co[self.cp[6]] + 1)
+
+            self.eo[self.ep[1]] = (self.eo[self.ep[1]] + 1) % 2
+            self.eo[self.ep[2]] = (self.eo[self.ep[2]] + 1) % 2
+            self.eo[self.ep[9]] = (self.eo[self.ep[9]] + 1) % 2
+            self.eo[self.ep[10]] = (self.eo[self.ep[10]] + 1) % 2
+
         match move:
-            case 'U': u_clockwise()
-            case 'U2': [u_clockwise() for _ in range(2)]
-            case 'U\'': [u_clockwise() for _ in range(3)]
-            case 'D': d_clockwise()
-            case 'D2': [d_clockwise() for _ in range(2)]
-            case 'D\'': [d_clockwise() for _ in range(3)]
             case 'L': l_clockwise()
             case 'L2': [l_clockwise() for _ in range(2)]
             case 'L\'': [l_clockwise() for _ in range(3)]
@@ -150,33 +141,112 @@ class State:  # rename like rubik state
             case 'B': b_clockwise()
             case 'B2': [b_clockwise() for _ in range(2)]
             case 'B\'': [b_clockwise() for _ in range(3)]
-            case _: raise Exception  # make another except
+            case 'U': u_clockwise()
+            case 'U2': [u_clockwise() for _ in range(2)]
+            case 'U\'': [u_clockwise() for _ in range(3)]
+            case 'D': d_clockwise()
+            case 'D2': [d_clockwise() for _ in range(2)]
+            case 'D\'': [d_clockwise() for _ in range(3)]
+            case _: raise Exception  # make another except # delete it
 
-    def h_cost(self, stage='stage_0'):  # input list
+    def h_cost(self):  # input list
         # maybe property  # reaname it # or get h cost
         # def weight_stage_cepo(self, stage):  # input list
         # rashiftovke of corner pern edge orientations
         # count number of not zeros?
-        if stage == 'stage_0':
-            return 12 - self.eo.count(0)  # example 12 - 9 zeros = 3
-        elif stage == 'stage_1':
-            # плюс последние 8 9 9 10 11 для боковух
-            return 20 - self.eo.count(0) - self.co.count(0) # edges and corners orientations
+        # middle edges to middle layer - article page 14 begin
 
+        # print(self.stage)
+        # exit()
+        # if self.stage == 'stage_0':
+        #     kek = 0
+        #     for i in range(8):
+        #         if self.cp[i] == i:
+        #             kek += 1
+        #     return 16 - self.co.count(0) - kek
+            # return 8 - self.сo.count(0)
+
+        heur = 0
+        if self.stage == 'stage_0':
+            for i in [0, 3, 8, 11]:
+                if self.ep[i] == i:
+                    heur += 1
+                if self.eo[i] == 0:
+                    heur += 1
+            # for i in [0, 3, 8, 11]:
+            #     if self.eo[i] == 0:
+            #         heur += 1
+
+            # for i in [0, 3, 4, 7]:
+            #     if self.cp[i] == i:
+            #         heur += 1
+
+            return 8 - heur
+
+
+        if self.stage == 'stage_01':
+            return 12 - self.eo.count(0)  # example 12 - 9 zeros = 3
+        elif self.stage == 'stage_12':
+            # lr slise means layer btw L and R sides. e8 9 10 11 edges by indexing
+            # плюс последние 8 9 10 11 для боковух
+            lr_slice_heuristic = 0
+            # print('!!!edge_perm', self.ep[-4:]) # lr_edges
+            for edge_permutation in self.ep[-4:]:
+                if edge_permutation in [8, 9, 10, 11]:
+                    lr_slice_heuristic += 2
+            return 16 - self.co.count(0) - lr_slice_heuristic
+            # if sorted(self.ep[-4:]) == [8, 9, 10, 11]:
+            #     return 0  # edges and corners orientations
+            # return 1
+        elif self.stage == 'stage_11':
+            # print('stage 2 is here')
+            lr_slice_heuristic = 0
+            # print('!!!edge_perm', self.ep[-4:]) # lr_edges
+            for edge_permutation in self.ep[-4:]:
+                if edge_permutation in [8, 9, 10, 11]:
+                    lr_slice_heuristic += 1
+            # if sorted(self.ep[-4:]) == [8, 9, 10, 11]:
+            #     lr_slice_heuristic = 1
+                  # edges and corners orientations
+            # lr_slice_heuristic = lr_slice_heuristic // 4
+            # print(lr_slice_heuristic)
+            # return 9 - self.co.count(0) - lr_slice_heuristic
+            # return 1 - lr_slice_heuristic  # edges and corners orientations
+            return 12 - self.co.count(0) - lr_slice_heuristic
+            # return 9 - self.co.count(0) - lr_slice_heuristic
+            # return 8 - self.co.count(0)
+
+        """
+        print('lr_slice_heuristic', lr_slice_heuristic)
+        print('self.ep[-4:]:', self.ep[-4:])
+        print("HH", 12 - self.co.count(0) - lr_slice_heuristic)
+        print(self.notation)
+        print(self)
+        print(self.properties)
+        print(self.notation_history)
+        print()
+        """
+            # print('lr_slice_heuristic', lr_slice_heuristic)
+            # print('HEUR', 24 - self.eo.count(0) - self.co.count(0) - lr_slice_heuristic)
+            # print(self)
+            # return 24 - self.eo.count(0) - self.co.count(0) - lr_slice_heuristic  # edges and corners orientations
+            # return 12 - self.co.count(0) - lr_slice_heuristic  # edges and corners orientations
+            # return 8 - self.co.count(0)  # edges and corners orientations
+            # return 20 - self.eo.count(0) - self.co.count(0)
 
     def f_cost(self):
         """ len of history shows g(n). distance in steps from the initial node """
         return len(self.notation_history) + self.h_cost()
-        pass
+        # return self.h_cost()
 
-    def is_goal_stage(self, stage):
+    def is_goal_stage(self): # is goal stage state
         """make 12 not hardcoded"""
         # print(gre(self.cost('stage_0')))
+        return self.h_cost() == 0
 
-        if stage == 'stage_0':
-            return self.h_cost('stage_0') == 0
-        elif stage == 'stage_1':
-            return
+        # if self.stage == 'stage_0':
+        # elif self.stage == 'stage_1': # maybe it will be unnecessary
+        #     return
 
     def __lt__(self, other: State) -> bool: # add here whitch state is compre
         return self.f_cost() < other.f_cost()
@@ -184,12 +254,15 @@ class State:  # rename like rubik state
         # return self.h_cost(st) + len(self.notation_history) < other.h_cost(st) + len(other.notation_history)
 
     def __str__(self):
-        # can make it more beauty with numpy
-        print(gre(f"corner_permutation: {self.properties['corner_permutation']}"))
-        print(gre(f"corner_orientation: {self.properties['corner_orientation']}"))
-        print(yll(f"edge_permutation: {self.properties['edge_permutation']}"))
-        print(yll(f"edge_orientation: {self.properties['edge_orientation']}"))
-        return ''
+        # print(gre(f"corner_permutation: {self.properties['corner_permutation']}"))
+        # print(gre(f"corner_orientation: {self.properties['corner_orientation']}"))
+        # print(yll(f"edge_permutation: {self.properties['edge_permutation']}"))
+        # print(yll(f"edge_orientation: {self.properties['edge_orientation']}"))
+        str_dict = {k: ' '.join(map(str, v)) for k, v in self.properties.items()}
+        data = {'permutation': [str_dict['corner_permutation'], str_dict['edge_permutation']],
+                'orientation': [str_dict['corner_orientation'], str_dict['edge_orientation']]}
+        df = pd.DataFrame.from_dict(data, orient='index').rename(columns={0: 'corner', 1: 'edge'})
+        return df.to_string()
 
     @staticmethod
     def calculate_new_co(corner_orientation):
--- solver.py
+++ solver.py
@@ -5,6 +5,7 @@
 # print(st)
 # add timer
 # make in rubik moves in good order like subgroup state
+# try solved orientaion to solve
 
 import os
 import re
@@ -25,7 +26,7 @@ log = lambda i: logging.info(i)
 gre = lambda i: colored(i, 'green')
 yll = lambda i: colored(i, 'yellow')
 
-test = {  # maybe name it in full
+target = {  # maybe name it in full
     'corner_permutation': list(range(8)),
     'edge_permutation': list(range(12)),
     'corner_orientation': [0] * 8,
@@ -53,41 +54,64 @@ RUru = { # maybe name it in full
     'edge_orientation': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1]
 }
 
-stages = ['stage_0', 'stage_1']  # rename to stage 0
+stages = ['stage_0']#, 'stage_1']#, 'stage_2']  # rename to stage 0
+# stages = ['stage_0', 'stage_1']#, 'stage_2']  # rename to stage 0
 
 # g0 = {'R', 'B', 'D'}
 # g0 = {'R', 'B', 'D', 'U', 'L'}
 # g0 = {'L', 'R', 'F', 'B', 'U', 'D'}
 # g0 = ['L', 'R', 'F', 'B', 'U', 'D']  # anke more notations
-g0 = ['L', 'R', 'F', 'B', 'U', 'D']  # anke more notations
+# g0 = ['L', 'R', 'F', 'B', 'U', 'D']  # anke more notations
 # g0 = ['L', 'R', 'F', 'B', 'U', 'D', 'L\'', 'R\'', 'F\'', 'B\'', 'U\'', 'D\'']  # anke more notations
+# g0 =["U", "U'", "U2", "D", "D'", "D2", "R", "R'", "R2", "L", "L'", "L2", "F", "F'", "F2", "B", "B'", "B2"]
 
 config_dict = {
     '3x3x3': {
         # goal state ??
         # 'subgroup_0_moves': ['U', 'D', 'L', 'R', 'F', 'B'],
-        'subgroup_0_moves': ['L', 'R', 'F', 'B', 'U', 'D'],
+        # 'subgroup_0_moves': ['L', 'R', 'F', 'B', 'U', 'D'],
+        # 'stage_0_moves': ['L', 'R', 'F', 'B', 'U', 'D'],
+        'stage_0_moves': ["U", "U'", "U2", "D", "D'", "D2", "R", "R'", "R2", "L", "L'", "L2", "F", "F'", "F2", "B", "B'", "B2"],
+        'stage_1_moves': ['L', 'R', 'F', 'B', 'U2', 'D2'],
+        'stage_2_moves': ['L', 'R', 'F', 'B', 'U2', 'D2'],
     }
 }
 
+# n = {
+#     'corner_permutation': [7, 6, 4, 1, 3, 2, 0, 5],
+#     'edge_permutation': [3, 5, 2, 6, 0, 9, 10, 11, 4, 7, 8, 1],
+#     'corner_orientation': [0, 0, 1, 1, 0, 1, 0, 0],
+#     'edge_orientation': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# }
+# kek = State(n, None, None, 'stage_0')
+# kek.moves('L')
+# print(kek)
+
+# print(int('011000000110', 2))
 
 class Solver:
     def __init__(self, shuffled_node):  # it is the biging state
-        self.start_node = shuffled_node # this node will be over init for each stage
+        self.start_node = shuffled_node  # this node will be over init for each stage
         self.open_list = PriorityQueue()
         self.solution_path = list()
 
         # for stage in ['stage_0', 'stage_1', 'stage_2', 'stage_3']:
         # for stage in ['stage_1']:
-        for stage in ['stage_0']:
-            pass
-        self.start_solve() # start solves group
+        # for stage in ['stage_0', 'stage_1']:
+        for stage in range(len(stages)):  # num stage
+            self.start_solve(stage)  # start solves group
+            # print('SIZe OF QUE', self.open_list.qsize())
+            self.open_list = PriorityQueue()
+            # print('SIZe OF QUE', self.open_list.qsize())
+            # exit()
+
         # обновить старт ноду для исполнения
         print('end')
         print(self.solution_path)
 
-    def start_solve(self):
+    def start_solve(self, stage):
         threshold = self.start_node.f_cost()
+        self.open_list = PriorityQueue()  # need i?
         solved = False  # solved state
         ii = 0
         while solved is False:
@@ -99,19 +123,28 @@ class Solver:
             while self.open_list.qsize():
                 lowest_node = self.open_list.get()
 
-                if lowest_node.is_goal_stage('stage_0'):
+                if lowest_node.is_goal_stage():
                     print(gre('BiNGO! the answer is here'))
+                    print(gre(lowest_node))
                     print(gre(lowest_node.notation))
                     print(gre(lowest_node.notation_history))
                     print()
-                    for k, v in lowest_node.properties.items():
-                        print(k, v)
+                    # for k, v in lowest_node.properties.items():
+                    #     print(k, v)
                     self.solution_path += lowest_node.notation_history
                     solved = True
+                    # print('id of lowest', id(lowest_node))
+                    self.start_node = State(
+                        # copy.deepcopy(lowest_node.properties), f'stage_{stage + 1}', None, None
+                        lowest_node.properties, f'stage_{stage + 1}', None, None
+                    )
                     break
 
-                for notation in g0:
-                    new_state = State(lowest_node.properties, notation, # rename move history
+                # for notation in g0:
+                for notation in config_dict['3x3x3'][f'stage_{stage}_moves']:
+                    # print(stage)
+                    # print(config_dict['3x3x3'][f'{stage}_moves'])
+                    new_state = State(lowest_node.properties, stages[stage], notation,  # rename move history
                                       copy.deepcopy(lowest_node.notation_history))
                     # print(f'node cost {new_state.f_cost()}, '
                     #       f'{new_state.notation}, {new_state.notation_history}')
@@ -119,23 +152,117 @@ class Solver:
                         self.open_list.put(new_state)
                     else:
                         thresholds.append(new_state.f_cost())
-            if solved:
+            if solved is True:
                 break
             print()
-            log(f"thresholds {thresholds}")
+            # log(f"thresholds {thresholds}")
             # log(f'end while | size que {self.open_list.qsize()}')
             threshold = min(thresholds)
             log(f"threshold after all : {threshold}")
             log('')
             # if ii > 7:
             #     exit()
-
             ii += 1
+            # if stage == 'stage_1':
+            #     exit()
         print('NUM ITERS IS', ii)
 
 
-shuf = State(qwerty, None, None)
+shuf = State(qwerty, None, None, None)
+# shuf = State(RUru, None, None, None)
 # shuf = State(test, None, None)
 # print(shuf.h_cost())
 
 solv = Solver(shuf)
+
+
+
+
+"""
+exit()
+import queue
+
+
+table0 = [0] * 2048
+# print(table0)
+ini = State(target, None, None, None)
+parents = queue.Queue()
+parents.put(ini)
+# print(parents.qsize())
+# depth = 6
+# print('\nstart')
+# for depth in range(1, 6):
+#     print('depth', depth)
+#
+#     new_node = parents.get()
+#     for notation in g0:
+#         new_state = State(new_node.properties, None, notation,
+#                           copy.deepcopy(new_node.notation_history))
+#         parents.put(new_state)
+#         # print(new_state)
+#         # print(new_state.eo)
+#         prep_str = ''.join(map(str, new_state.eo[:-1]))
+#         # print('prep_str =', prep_str)
+#         str_eo = int(prep_str, 2)
+#         # print(str_eo)
+#         if str_eo != 0:
+#             print(str_eo)
+#         if not table0[str_eo]:
+#             table0[str_eo] = depth
+#         # print(type(new_state.eo))
+# print(table0)
+for depth in range(1, 171):
+    parent = parents.get()
+    for move in g0:
+        child = State(
+            copy.deepcopy(parent.properties),
+            None, move, None  #parent.notation_history
+        )
+        index = int(''.join(map(str, child.eo[:-1])), 2)
+        # print(index)
+        if index != 0 and table0[index] == 0:
+            table0[index] = depth
+        parents.put(child)
+    # print('size', parents.qsize())
+    # parents = children
+# }
+# for i, depth := range table {
+#     if i > 0 && depth == 0 {
+#         table[i] = 7
+#     }
+# }
+# print(table0)
+# print(table0.count(1))
+# print(set(table0))
+func tableG0() [2048]uint8 {
+    fmt.Printf("\nGenerating pruning table for G0")
+    var table [2048]uint8
+    var parents []cepo
+    parents = append(parents, *initCube())
+    var depth uint8
+    for depth < 6 {
+        var children []cepo
+        depth++
+        for _, parent := range parents {
+            for _, move := range listMoves(&parent, 0) {
+                child := newNode(&parent, move)
+                spin(move, child)
+                index := binaryToDecimal(child.eO)
+                if index != 0 && table[index] == 0 {
+                    table[index] = depth
+                }
+                children = append(children, *child)
+            }
+        }
+        parents = children
+        fmt.Printf(".")
+    }
+    for i, depth := range table {
+        if i > 0 && depth == 0 {
+            table[i] = 7
+        }
+    }
+    return table
+}
+"""
+
